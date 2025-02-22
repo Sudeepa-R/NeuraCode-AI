9c50546de0e6d446c6c870954fc0f5c49fa09652
Sudeepa-R <sudeepar084@gmail.com>
2025-02-22 12:55:39 +0530

    contact page created

--- src/shared-components/router/router.tsx
+++ src/shared-components/router/router.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "../../dashboard";
 import PackagesIndex from "../../dashboard/packages";
+import ContactIndex from "../../dashboard/contact";
 
 const AppRoutes = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Dashboard/>} />
-          <Route path="/packages" element={<PackagesIndex/>} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/packages" element={<PackagesIndex />} />
+          <Route path="/contactUs" element={<ContactIndex />} />
         </Routes>
       </Router>
     </div>
